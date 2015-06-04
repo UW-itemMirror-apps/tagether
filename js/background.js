6902bc6947762015970b276f0a8a30dacf30047d
jayxu66 <jayx66@uw.edu>
2015-06-04 11:45:52 -0700

    Added the new function to find folders

--- js/background.js
+++ js/background.js
@@ -144,98 +144,7 @@ function execute(msg, sender) {
 	}
 }
 
-//New filter keywords function
-function filterKeyword(text) {
-	var regex_str;
-	var regex;
-	var stopWords = ["a", "about", "above", "above", "across", "after",
-    "afterwards", "again", "against", "all", "almost", "alone", "along",
-    "already", "also","although","always","am","among", "amongst",
-    "amoungst", "amount",  "an", "and", "another", "any","anyhow","anyone",
-    "anything","anyway", "anywhere", "are", "around", "as",  "at", "back",
-    "be","became", "because","become","becomes", "becoming", "been",
-    "before", "beforehand", "behind", "being", "below", "beside",
-    "besides", "between", "beyond", "bill", "both", "bottom","but",
-    "by", "call", "can", "cannot", "cant", "co", "con", "could",
-    "couldnt", "cry", "de", "describe", "detail",  "do", "does", "done",  "down",
-    "due", "during", "each", "eg", "eight", "either", "eleven","else",
-    "elsewhere", "empty", "enough", "etc", "even", "ever", "every",
-    "everyone", "everything", "everywhere", "except", "few", "fifteen",
-    "fify", "fill", "find", "fire", "first", "five", "for", "former",
-    "formerly", "forty", "found", "four", "from", "front", "full",
-    "further", "get", "give", "go", "had", "has", "hasnt", "have",
-    "he", "hence", "her", "here", "hereafter", "hereby", "herein",
-    "hereupon", "hers", "herself", "him", "himself", "his", "how",
-    "however", "hundred", "ie", "if", "in", "inc", "indeed",
-    "interest", "into", "is",  "it", "its", "itself", "keep",
-    "last", "latter", "latterly", "least", "less", "ltd", "made",
-    "many", "may", "me", "meanwhile", "might", "mill", "mine",
-    "more", "moreover", "most", "mostly", "move", "much", "must", "my",
-    "myself", "name", "namely", "neither", "never", "nevertheless", "next",
-    "nine", "no", "nobody", "none", "noone", "nor", "not", "nothing", "now",
-    "nowhere", "of", "off", "often", "on", "once", "one", "only", "onto",
-    "or", "other", "others", "otherwise", "our", "ours", "ourselves", "out",
-    "over", "own","part", "per", "perhaps", "please", "put", "rather", "re",
-    "same", "see", "seem", "seemed", "seeming", "seems", "serious", "several",
-    "she", "should", "show", "side", "since", "sincere", "six", "sixty", "so",
-    "some", "somehow", "someone", "something", "sometime", "sometimes", "somewhere",
-    "still", "such", "system", "take", "ten", "than", "that", "the", "their", "them",
-    "themselves", "then", "thence", "there", "thereafter", "thereby", "therefore",
-    "therein", "thereupon", "these", "they", "thickv", "thin", "third", "this",
-    "those", "though", "three", "through", "throughout", "thru", "thus", "to",
-    "together", "too", "top", "toward", "towards", "twelve", "twenty", "two",
-    "un", "under", "until", "up", "upon", "us", "very", "via", "was", "way", "we",
-    "well", "were", "what", "whatever", "when", "whence", "whenever", "where",
-    "whereafter", "whereas", "whereby", "wherein", "whereupon", "wherever",
-    "whether", "which", "while", "whither", "who", "whoever", "whole", "whom",
-    "whose", "why", "will", "with", "within", "without", "would", "yet", "you",
-    "your", "yours", "yourself", "yourselves", "the",
-    // contractions?
-    "didnt", "doesnt", "dont", "isnt", "wasnt", "youre", "hes", "ive", "theyll",
-    "whos", "wheres", "whens", "whys", "hows", "whats", "were", "shes", "im", "thats"
-    ];
-    var cleansed_string = text;
-
-    // Split out all the individual words in the phrase
-    var words = cleansed_string.match(/[^\s]+|\s+[^\s+]$/g);
-
-    var keywords = [];
-
-    // Review all the words
-    for (var x=0; x < words.length; x++) {
-            // For each word, check all the stop words
-            for (var y=0; y < stopWords.length; y++) {
-                    // Get the current word
-                    var word = words[x].replace(/\s+|[^a-z]+/ig, "");       // Trim the word and remove non-alpha
-
-                    // Get the stop word
-                    var stopword = stopWords[y];
-
-                    // If the word matches the stop word, remove it from the keywords
-                    if (word.toLowerCase() == stopword) {
-                            // Build the regex
-                            regex_str = "^\\s*"+stopword+"\\s*$";           // Only word
-                            regex_str += "|^\\s*"+stopword+"\\s+";          // First word
-                            regex_str += "|\\s+"+stopword+"\\s*$";          // Last word
-                            regex_str += "|\\s+"+stopword+"\\s+";           // Word somewhere in the middle
-                            regex = new RegExp(regex_str, "ig");
-
-                            // Remove the word from the keywords
-                            cleansed_string = cleansed_string.replace(regex, " ");
-                    }
-            }
-    }
-
-    var keywordsFound = cleansed_string.split(' ');
-    for (var i = 0; i < keywordsFound.length; i++) {
-            if (keywordsFound[i] !== "" && keywords.indexOf(keywordsFound[i]) === -1) {
-                    keywords.push(keywordsFound[i]);
-            }
-    }
-
-    return keywords;
-}
-
+//function to identify the stopwords
 function findStopwords(word){
 	var stopWords = ["a","able","about","across","after","all","almost","also","am","among","an","and","any","are","as","at","be","because","been","but","by","can","cannot","could","dear","did","do","does","either","else","ever","every","for","from","get","got","had","has","have","he","her","hers","him","his","how","however","i","if","in","into","is","it","its","just","least","let","like","likely","may","me","might","most","must","my","neither","no","nor","not","of","off","often","on","only","or","other","our","own","rather","said","say","says","she","should","since","so","some","than","that","the","their","them","then","there","these","they","this","tis","to","too","twas","us","wants","was","we","were","what","when","where","which","while","who","whom","why","will","with","would","yet","you","your"];
 	if(word === "a"){
@@ -249,7 +158,7 @@ function findStopwords(word){
 }
 
 //function to find files match a name pattern
-function findSomeFiles(words, tab) {  //words need to change to paragraph
+function findSomeFiles(words, tab) {  
 	var resultSet = {};
 	var initResult = [];
 	//filter keyword using stopwords
@@ -258,9 +167,6 @@ function findSomeFiles(words, tab) {  //words need to change to paragraph
 	console.log('charList: %o', charList);
 	var length = charList.length;
 	var keyword = '';
-
-	
-
 
 	var mark = 0;
 	for(var c=0; c<length; c++){
@@ -284,32 +190,6 @@ function findSomeFiles(words, tab) {  //words need to change to paragraph
 	console.log('mark: %o', mark);
 
 	var formerWordResult = dropboxSearch(keyword, resultSet, mark, stopwordFlag, charList, tab, initResult);
-
-	// if(!stopwordFlag){
-	// 	var formerWordResult = dropboxSearch(keyword, resultSet, mark, charList, tab, initResult);
-	// }else {
-	// 		var arr = grabword(charList, mark, '');
-
-	// 		var keyword = arr[0];
-
-	// 		var start = arr[1];
-
-	// 		var flag = arr[2];
-
-	// 		if(!flag) {
-	// 			dropboxSearch(keyword, resultSet, start, charList, tab, initResult);
-	// 		}else {
-	// 			var arr = grabword(charList, start, '');
-
-	// 			var keyword = arr[0];
-
-	// 			var start1 = arr[1];
-
-	// 			var flag2 = arr[2];
-	// 		}
-	// }
-	
-	//console.log('resultSet: %o', resultSet);
 
 	console.log('formerWordResult: %o', formerWordResult);
 
